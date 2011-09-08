8384950ac2360c37c58fb680779ccd9d1a339b29
Ron Bodkin <rbodkin@thinkbiganalytics.com>
2011-09-08 12:02:40 -0700

    Optimize UDF matching for performance

--- src/main/java/thinkbig/hive/udf/MatchUDFUtil.java
+++ src/main/java/thinkbig/hive/udf/MatchUDFUtil.java
@@ -21,241 +21,277 @@
 package thinkbig.hive.udf;
 
 import java.text.BreakIterator;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
-public class MatchUDFUtil
-{
-	private static final String PUNCTUATION = "[;\\.\\-_,\\|\\&\\%\\*\\?\\!\\^\\~\\s+\\[\\]\\+/'\"@#=\\>\\<\\(\\)\\{\\}\\:]+";
-	
-	/* private enum AlgoType
-	{
-		  KEYWORD,		// exact match
-		  EDITDISTANCE, // approx match
-		  LCS,			// approx match
-		  LCSTR,		// approx match
-		  REGEX,		// regex match
-		  KMP,
-		  BOYERMOORE,
-		  RABINKARP,
-		  SUFFIXTREE,
-		  AHOCORASICK 	//...
-	} */
-	
-	public enum MatchType
-	{
-		SomeWords,	// check if at least one word in the pattern matches (OR)
-		AllWords,	// check if at least one word in the pattern matches (AND)
-		WholePhrase // check if the entire phrase matches
-	}
+public class MatchUDFUtil {
+    private static final String PUNCTUATION = "[;\\.\\-_,\\|\\&\\%\\*\\?\\!\\^\\~\\s+\\[\\]\\+/'\"@#=\\>\\<\\(\\)\\{\\}\\:]+";
 
-	/**
-	 * gets the actual algorithm class
-	 * @param algoType
-	 * @return the concrete algorithm class
-	 */
-	public static MatchAlgorithm getAlgo(String algoType) {
-		 
-		    MatchAlgorithm algo = null;
-			
-		    algoType = algoType == null ? "keyword" : algoType;
-		    
-		    if (algoType.equalsIgnoreCase("regex")) {
-				algo = new RegexMatch();
-		    }
-			else if (algoType.equalsIgnoreCase("edit")) {
-				algo = new EditDistanceMatch();
-		    }
-			else if (algoType.equalsIgnoreCase("lcstr")) {
-				algo = new LCStrMatch();
-		    }
-			else if (algoType.equalsIgnoreCase("lcs")) {
-				algo = new LCSMatch();
-		    }
-			else { // default is "keyword"
-				algo = new KeywordBasedMatch();
-		    }
-		    return algo;
-	}
-	
-	public static boolean match(String text, String pattern) {
-		return match(text, pattern, "keyword", MatchType.SomeWords);
-	}
-	
-	public static boolean match(String text, String pattern, String algoType, MatchType matchType) {
-		return match(text, pattern, algoType, matchType, true);	// use sentence type split using BackIterator
-	}
-	
-	// TODO: should use enum for algorithm type
-	// then use Enum.valueOf for String to enum conversion
-	/**
-	 * matches text with pattern
-	 * @param text
-	 * @param pattern
-	 * @param algoType
-	 * @param matchType
-	 * @return true is found a match, false otherwise
-	 */
-	public static boolean match(String text, String pattern, String algoType, MatchType matchType,
-											 boolean sentenceStyle) {
-		
-		algoType = algoType == null ? "keyword" : algoType;	    
-	    	MatchAlgorithm algo = getAlgo(algoType);
+    /*
+     * private enum AlgoType { KEYWORD, // exact match EDITDISTANCE, // approx match LCS, // approx match LCSTR, // approx match
+     * REGEX, // regex match KMP, BOYERMOORE, RABINKARP, SUFFIXTREE, AHOCORASICK //... }
+     */
 
-	    	// get rid of the punctuation from text
-    		if (!sentenceStyle) {
-		    	text = text.toLowerCase().replaceAll(PUNCTUATION, " "); 		
-	    	}
-	    	else {
-	    		String[] splits = splitSentence(text.toLowerCase(), Locale.ENGLISH);
-	    		text = "";
-	    		for (String textSplit : splits) {
-	    			text += textSplit + " ";
-	    		}
-	    	}
-    		
-		    if (!algoType.equalsIgnoreCase("regex")) {
-		    	// get rid of the punctuation from pattern
-	    		if (!sentenceStyle) {
-	    			pattern = pattern.toLowerCase().replaceAll(PUNCTUATION, " ");
-	    		}
-	    		else {
-		    		String[] splits = splitSentence(pattern.toLowerCase(), Locale.ENGLISH);
-		    		pattern = "";
-		    		for (String patSplit : splits) {
-		    			pattern += patSplit + " ";
-		    		}
-		    	} 
-		    }
-		    System.out.println("Text & Pattern: " + text + "& " + pattern);
-		    
-			boolean found = matchType == MatchType.AllWords? true : false;
-		    if (matchType == MatchType.WholePhrase) {
-		    	found = algo.match(text, pattern);
-		    }
-		    else { 	
-		    	String[] patterns = pattern.split(" ");
-		    	for (int i = 0;  i < patterns.length; ++i) {
-		    		if (matchType == MatchType.AllWords) {
-		    			found &= algo.match(text, patterns[i]);
-		    		}
-		    		else {	// default: check if at least one word in the pattern matches
-		    			found |= algo.match(text, patterns[i]);
-		    		}
-		    		// ToDo: add weights to words from the pattern
-				}
-		    }
-		    System.out.println(found);
-	    	return found;
-	}
-	
-	public static int countMatch(String text, String pattern) {
-		return countMatch(text, pattern, "keyword");
-	}
-	
-	// should use enum for algorithm type
-	public static int countMatch(String text, String pattern, String algoType) {
-		
-	    MatchAlgorithm algo = null;
-	    
-	    algoType = algoType == null ? "keyword" : algoType;
-	    
-	    if (algoType.equalsIgnoreCase("regex")) {
-			algo = new RegexMatch();
-	    }
-		else if (algoType.equalsIgnoreCase("edit")) {
-			algo = new EditDistanceMatch();
-	    }
-		else if (algoType.equalsIgnoreCase("lcstr")) {
-			algo = new LCStrMatch();
-	    }
-		else if (algoType.equalsIgnoreCase("lcs")) {
-			algo = new LCSMatch();
-	    }
-		else { // default is "keyword"
-			algo = new KeywordBasedMatch();
-	    }
-	    
-	    text = text.replaceAll(PUNCTUATION, " ");
-	    System.out.print("Text & Pattern: " + text + "& " + pattern);
-		
-		return algo == null ? 0 : algo.countMatch(text.toLowerCase(), pattern.toLowerCase());
-	}
-	
-	 /**
+    public enum MatchType {
+        SomeWords, // check if at least one word in the pattern matches (OR)
+        AllWords, // check if at least one word in the pattern matches (AND)
+        WholePhrase
+        // check if the entire phrase matches
+    }
+
+    /**
+     * gets the actual algorithm class
+     * 
+     * @param algoType
+     * @return the concrete algorithm class
+     */
+    public static MatchAlgorithm getAlgo(String algoType) {
+
+        MatchAlgorithm algo = null;
+
+        algoType = algoType == null ? "keyword" : algoType;
+
+        if (algoType.equalsIgnoreCase("regex")) {
+            algo = new RegexMatch();
+        }
+        else if (algoType.equalsIgnoreCase("edit")) {
+            algo = new EditDistanceMatch();
+        }
+        else if (algoType.equalsIgnoreCase("lcstr")) {
+            algo = new LCStrMatch();
+        }
+        else if (algoType.equalsIgnoreCase("lcs")) {
+            algo = new LCSMatch();
+        }
+        else { // default is "keyword"
+            algo = new KeywordBasedMatch();
+        }
+        return algo;
+    }
+
+    public static boolean match(String text, String pattern) {
+        return match(text, pattern, "keyword", MatchType.SomeWords);
+    }
+
+    public static boolean match(String text, String pattern, String algoType, MatchType matchType) {
+        return match(text, pattern, algoType, matchType, true); // use sentence type split using BackIterator
+    }
+
+    // TODO: should use enum for algorithm type
+    // then use Enum.valueOf for String to enum conversion
+    /**
+     * matches text with pattern
+     * 
+     * @param text
+     * @param pattern
+     * @param algoType
+     * @param matchType
+     * @return true is found a match, false otherwise
+     */
+    public static boolean match(String text, String pattern, String algoType, MatchType matchType, boolean sentenceStyle) {
+
+        algoType = algoType == null ? "keyword" : algoType;
+        MatchAlgorithm algo = getAlgo(algoType);
+
+        // get rid of the punctuation from text
+        if (!sentenceStyle) {
+            text = normalizeSpaces(text);            
+        }
+        else {
+            text = normalizeSentence(text);
+        }
+
+        if (!algoType.equalsIgnoreCase("regex")) {
+            // get rid of the punctuation from pattern
+            if (!sentenceStyle) {
+                pattern = pattern.toLowerCase().replaceAll(PUNCTUATION, " ");
+            }
+            else {
+                String[] splits = splitSentence(pattern.toLowerCase(), Locale.ENGLISH);
+                pattern = "";
+                for (String patSplit : splits) {
+                    pattern += patSplit + " ";
+                }
+            }
+        }
+        System.out.println("Text & Pattern: " + text + "& " + pattern);
+
+        boolean found = matchType == MatchType.AllWords ? true : false;
+        if (matchType == MatchType.WholePhrase) {
+            found = algo.match(text, pattern);
+        }
+        else {
+            String[] patterns = pattern.split(" ");
+            for (int i = 0; i < patterns.length; ++i) {
+                if (matchType == MatchType.AllWords) {
+                    found &= algo.match(text, patterns[i]);
+                }
+                else { // default: check if at least one word in the pattern matches
+                    found |= algo.match(text, patterns[i]);
+                }
+                // ToDo: add weights to words from the pattern
+            }
+        }
+        System.out.println(found);
+        return found;
+    }
+
+    // An LRU cache using a linked hash map
+    static class HashCache<K, V> extends LinkedHashMap<K, V> {
+
+      private static final int CACHE_SIZE = 32;
+      private static final int INIT_SIZE = 64;
+      private static final float LOAD_FACTOR = 0.6f;
+
+      HashCache() {
+        super(INIT_SIZE, LOAD_FACTOR);
+      }
+
+      private static final long serialVersionUID = 1;
+
+      @Override
+      protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size() > CACHE_SIZE;
+      }
+
+    }
+
+    static HashCache<String, String> spaceNormalized = new HashCache<String, String>();
+    static HashCache<String, String> sentenceNormalized = new HashCache<String, String>();
+    
+    private static String normalizeSpaces(String text) {
+        String normalized = spaceNormalized.get(text);
+        if (normalized == null) {
+            normalized = text.toLowerCase().replaceAll(PUNCTUATION, " ");
+            spaceNormalized.put(text, normalized);
+        }
+        return normalized;
+    }
+
+    private static String normalizeSentence(String text) {
+        String normalized = sentenceNormalized.get(text);
+        if (normalized == null) {        
+            String[] splits = splitSentence(text.toLowerCase(), Locale.ENGLISH);
+            StringBuilder holder = new StringBuilder();
+            for (String textSplit : splits) {
+                holder.append(textSplit);
+                holder.append(' ');
+            }
+            normalized = holder.toString();
+            sentenceNormalized.put(text, normalized);
+        }
+        return normalized;
+    }
+
+    public static int countMatch(String text, String pattern) {
+        return countMatch(text, pattern, "keyword");
+    }
+
+    // should use enum for algorithm type
+    public static int countMatch(String text, String pattern, String algoType) {
+
+        MatchAlgorithm algo = null;
+
+        algoType = algoType == null ? "keyword" : algoType;
+
+        if (algoType.equalsIgnoreCase("regex")) {
+            algo = new RegexMatch();
+        }
+        else if (algoType.equalsIgnoreCase("edit")) {
+            algo = new EditDistanceMatch();
+        }
+        else if (algoType.equalsIgnoreCase("lcstr")) {
+            algo = new LCStrMatch();
+        }
+        else if (algoType.equalsIgnoreCase("lcs")) {
+            algo = new LCSMatch();
+        }
+        else { // default is "keyword"
+            algo = new KeywordBasedMatch();
+        }
+
+        text = text.replaceAll(PUNCTUATION, " ");
+        System.out.print("Text & Pattern: " + text + "& " + pattern);
+
+        return algo == null ? 0 : algo.countMatch(text.toLowerCase(), pattern.toLowerCase());
+    }
+
+    /**
      * Split using BreakIterator
+     * 
      * @param text
      * @return
      */
-     public static String[] splitSentence(String text, Locale locale) {
-	   
-       if (text == null) {
-    	   return null;
-       }
-    	 
-       BreakIterator boundary = BreakIterator.getWordInstance(locale);
-	   boundary.setText(text);
-	   // note the words may contain punctuation characters, get rid of them  
-       List<String> words = new ArrayList<String>();
-       
-       // take care of the first word specially
-       for (int p =  boundary.first(); p < boundary.next(); p++) {
-           if (Character.isLetter(text.codePointAt(p))) {
-               // a valid word
-        	   String word = text.substring(boundary.first(), boundary.next());
-        	   //System.out.println(word);
-    		   words.add(word);
-        	   break;
-           }
-       }
-       
-       int pos = 0;
-       while (pos != BreakIterator.DONE) {
-    	   Boundary wb = getNextWord(pos, text);
-    	   if (wb.end != BreakIterator.DONE) {
-    		   String word = text.substring(wb.start, wb.end);
-    		   //System.out.println(word);
-    		   words.add(word);
-    	   }
-    	   pos = wb.end;
-       }
+    public static String[] splitSentence(String text, Locale locale) {
 
-       return (String[])words.toArray(new String[0]);
-     }
-     
-     /**
-      * represents word boundary
-      * @author sandipandey
-      *
-      */
-     private static class Boundary {
-    	int start, end;
-    	Boundary(int s, int e) {
-    		start = s;
-    		end = e;
-    	}
-     }
-     
-     /**
-      * uses heuristic to find whether the content between two boundaries is a word
-      * @param pos
-      * @param text
-      * @return next word start and end pos
-      */
-     public static Boundary getNextWord(int pos, String text) {
-         BreakIterator wordBoundary = BreakIterator.getWordInstance();
-         wordBoundary.setText(text);
-         int last = wordBoundary.following(pos);
-         int current = wordBoundary.next();
-         while (current != BreakIterator.DONE) {
-             for (int p = last; p < current; p++) {
-                 if (Character.isLetter(text.codePointAt(p)))
-                     return new Boundary(last, current);
-             }
-             last = current;
-             current = wordBoundary.next();
-         }
-         return new Boundary(last, BreakIterator.DONE);
-     }
+        if (text == null) {
+            return null;
+        }
+
+        BreakIterator boundary = BreakIterator.getWordInstance(locale);
+        boundary.setText(text);
+        // note the words may contain punctuation characters, get rid of them
+        List<String> words = new ArrayList<String>();
+
+        // take care of the first word specially
+        for (int p = boundary.first(); p < boundary.next(); p++) {
+            if (Character.isLetter(text.codePointAt(p))) {
+                // a valid word
+                String word = text.substring(boundary.first(), boundary.next());
+                // System.out.println(word);
+                words.add(word);
+                break;
+            }
+        }
+
+        int pos = 0;
+        while (pos != BreakIterator.DONE) {
+            Boundary wb = getNextWord(pos, text);
+            if (wb.end != BreakIterator.DONE) {
+                String word = text.substring(wb.start, wb.end);
+                // System.out.println(word);
+                words.add(word);
+            }
+            pos = wb.end;
+        }
+
+        return (String[]) words.toArray(new String[0]);
+    }
+
+    /**
+     * represents word boundary
+     * 
+     * @author sandipandey
+     * 
+     */
+    private static class Boundary {
+        int start, end;
+
+        Boundary(int s, int e) {
+            start = s;
+            end = e;
+        }
+    }
+
+    /**
+     * uses heuristic to find whether the content between two boundaries is a word
+     * 
+     * @param pos
+     * @param text
+     * @return next word start and end pos
+     */
+    public static Boundary getNextWord(int pos, String text) {
+        BreakIterator wordBoundary = BreakIterator.getWordInstance();
+        wordBoundary.setText(text);
+        int last = wordBoundary.following(pos);
+        int current = wordBoundary.next();
+        while (current != BreakIterator.DONE) {
+            for (int p = last; p < current; p++) {
+                if (Character.isLetter(text.codePointAt(p)))
+                    return new Boundary(last, current);
+            }
+            last = current;
+            current = wordBoundary.next();
+        }
+        return new Boundary(last, BreakIterator.DONE);
+    }
 }
